1ea9a752b7a75424e13a5b01885511a290f69d18
Merey Kaliyev <yeahmerey@mail.ru>
2025-12-11 12:42:09 +0500

    some styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,14 @@
+import "./Home.css";
+import myImg from "../../assets/images.png";
+
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <div className="home-container">
+      <img src={myImg} alt="" className="home-image" />
+
+      <div className="home-text">
+        <h1>Rick And Morty</h1>
+      </div>
+    </div>
+  );
 }
